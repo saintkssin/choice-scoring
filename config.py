7b4a23edc7271bfd0ae11bfbf9305ed225ac2b89
saintkssin <saint.kssin@gmail.com>
2026-05-25 13:18:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 DATE_TO = datetime.now().strftime("%Y-%m-%d 23:59:59")
 DATE_FROM = (datetime.now() - timedelta(days=14)).strftime("%Y-%m-%d 00:00:00")
 
-RINGOSTAT_API_KEY = "NjV7IhCV8MRQ2nuHrA0XXyDFC3e8vkQp"
-GEMINI_API_KEY = "AIzaSyB03eJo0s2FF0piDHXAGva66mXEUQMAado"
+RINGOSTAT_API_KEY = "YOUR_RINGOSTAT_API_KEY"
+GEMINI_API_KEY = "YOUR_GEMINI_API_KEY"
 MIN_DURATION = 60
 MANAGERS = ["Sporysh", "bondarchuk", "Horobets", "karpiuk"]
